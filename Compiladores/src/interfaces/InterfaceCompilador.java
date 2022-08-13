c12807c3f2f6fba5040b95016c55841383e0268b
zenialexandre <zenialexandre123@gmail.com>
2022-08-13 09:29:57 -0300

    status bar

--- Compiladores/src/interfaces/InterfaceCompilador.java
+++ Compiladores/src/interfaces/InterfaceCompilador.java
@@ -1,9 +1,19 @@
 package interfaces;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
+@SuppressWarnings("deprecation")
 public class InterfaceCompilador {
 
 	private JFrame frame;
@@ -36,8 +46,23 @@ public class InterfaceCompilador {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(200, 200, 910, 600);
+		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.createStatusBar(frame);
 	}
 
+	private void createStatusBar(JFrame frame) {
+		String directoryName = this.getCurrentDirectory();
+		JMenuBar statusBar = new JMenuBar();
+		JMenu statusBarMenu = new JMenu();
+		statusBarMenu.setLabel(directoryName);
+		frame.setLayout(new BorderLayout());
+		frame.add(statusBar, BorderLayout.SOUTH);
+		statusBar.add(statusBarMenu);
+	}
+	
+	private String getCurrentDirectory() {
+		return this.getClass().getClassLoader().getResource("").getPath();
+	}
 }
