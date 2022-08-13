622883ed0396e6626e4ea702c6c2cda9b686e4c3
zenialexandre <zenialexandre123@gmail.com>
2022-08-13 11:05:41 -0300

    starting to build the tools bar

--- Compiladores/src/interfaces/InterfaceCompilador.java
+++ Compiladores/src/interfaces/InterfaceCompilador.java
@@ -9,14 +9,13 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
 import javax.swing.JMenuItem;
 
-@SuppressWarnings("deprecation")
+//@SuppressWarnings("deprecation")
 public class InterfaceCompilador {
 
 	private JFrame frame;
+	private JMenuBar toolsBar;
 
 	/**
 	 * Launch the application.
@@ -46,7 +45,7 @@ public class InterfaceCompilador {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(500, 500, 910, 600);
+		frame.setBounds(500, 500, 1010, 600);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.createStatusBar(frame);
@@ -54,10 +53,10 @@ public class InterfaceCompilador {
 	}
 
 	private void createStatusBar(JFrame frame) {
-		String directoryName = this.getCurrentDirectory();
+		//String directoryName = this.getCurrentDirectory();
 		JMenuBar statusBar = new JMenuBar();
 		JMenu statusBarMenu = new JMenu();
-		statusBarMenu.setLabel(directoryName);
+		//statusBarMenu.setLabel(directoryName);
 		frame.getContentPane().setLayout(new BorderLayout());
 		frame.getContentPane().add(statusBar, BorderLayout.SOUTH);
 		statusBar.add(statusBarMenu);
@@ -65,22 +64,71 @@ public class InterfaceCompilador {
 	}
 
 	private void createToolsBar(JFrame frame) {
-		JMenuBar toolsBar = new JMenuBar();
+		toolsBar = new JMenuBar();
 		frame.setJMenuBar(toolsBar);
 		toolsBar.setPreferredSize(new Dimension(900, 75));
 		this.createToolsBarItems(toolsBar);
 	}
 
 	private void createToolsBarItems(JMenuBar toolsBar) {
-		JMenuItem newFile = new JMenuItem("Abrir [ctrl + n]");
-		newFile.setHorizontalAlignment(SwingConstants.LEFT);
-		Image newFileIcon = new ImageIcon(this.getClass().getResource("/new_file_icon.png")).getImage();
-		toolsBar.add(newFile);
-		newFile.setIcon(new ImageIcon(newFileIcon));
-		newFile.setPreferredSize(new Dimension(10, 10));
+		{
+			JMenuItem newFileItem = new JMenuItem("Abrir [ctrl + n]");
+			Image newFileIcon = new ImageIcon(this.getClass().getResource("/new_file_icon.png")).getImage();
+			toolsBar.add(newFileItem);
+			newFileItem.setIcon(new ImageIcon(newFileIcon));
+		}
+
+		{
+			JMenuItem openFileItem = new JMenuItem("Abrir [ctrl + o]");
+			Image openFileIcon = new ImageIcon(this.getClass().getResource("/open_file_icon.png")).getImage();
+			toolsBar.add(openFileItem);
+			openFileItem.setIcon(new ImageIcon(openFileIcon));
+		}
+
+		{
+			JMenuItem saveFileItem = new JMenuItem("Salvar [ctrl + s]");
+			Image saveIcon = new ImageIcon(this.getClass().getResource("/save_icon.png")).getImage();
+			toolsBar.add(saveFileItem);
+			saveFileItem.setIcon(new ImageIcon(saveIcon));
+		}
+
+		{
+			JMenuItem copyItem = new JMenuItem("Copiar [ctrl + c]");
+			Image copyIcon = new ImageIcon(this.getClass().getResource("/copy_icon.png")).getImage();
+			toolsBar.add(copyItem);
+			copyItem.setIcon(new ImageIcon(copyIcon));
+		}
+
+		{
+			JMenuItem pasteItem = new JMenuItem("Colar [ctrl + v]");
+			Image pasteIcon = new ImageIcon(this.getClass().getResource("/paste_icon.png")).getImage();
+			toolsBar.add(pasteItem);
+			pasteItem.setIcon(new ImageIcon(pasteIcon));
+		}
+
+		{
+			JMenuItem cutItem = new JMenuItem("Recortar [ctrl + x]");
+			Image cutIcon= new ImageIcon(this.getClass().getResource("/cut_icon.png")).getImage();
+			toolsBar.add(cutItem);
+			cutItem.setIcon(new ImageIcon(cutIcon));
+		}
+
+		{
+			JMenuItem compileItem = new JMenuItem("Compilar [F7]");
+			Image compíleIcon = new ImageIcon(this.getClass().getResource("/compile_icon.png")).getImage();
+			toolsBar.add(compileItem);
+			compileItem.setIcon(new ImageIcon(compíleIcon));
+		}
+
+		{
+			JMenuItem groupItem = new JMenuItem("Equipe [F1]");
+			Image groupIcon = new ImageIcon(this.getClass().getResource("/group_icon.png")).getImage();
+			toolsBar.add(groupItem);
+			groupItem.setIcon(new ImageIcon(groupIcon));
+		}
 	}
 
-	private String getCurrentDirectory() {
+	/*private String getCurrentDirectory() {
 		return this.getClass().getClassLoader().getResource("").getPath();
-	}
+	}*/
 }
