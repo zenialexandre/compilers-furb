77d06771ce15f208a46e18f501aba403a84c9fb6
zenialexandre <zenialexandre123@gmail.com>
2022-08-13 10:36:01 -0300

    folder with images

--- Compiladores/src/interfaces/InterfaceCompilador.java
+++ Compiladores/src/interfaces/InterfaceCompilador.java
@@ -3,14 +3,14 @@ package interfaces;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.io.File;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 import javax.swing.JMenuItem;
 
 @SuppressWarnings("deprecation")
@@ -46,10 +46,11 @@ public class InterfaceCompilador {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(200, 200, 910, 600);
+		frame.setBounds(500, 500, 910, 600);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.createStatusBar(frame);
+		this.createToolsBar(frame);
 	}
 
 	private void createStatusBar(JFrame frame) {
@@ -57,11 +58,28 @@ public class InterfaceCompilador {
 		JMenuBar statusBar = new JMenuBar();
 		JMenu statusBarMenu = new JMenu();
 		statusBarMenu.setLabel(directoryName);
-		frame.setLayout(new BorderLayout());
-		frame.add(statusBar, BorderLayout.SOUTH);
+		frame.getContentPane().setLayout(new BorderLayout());
+		frame.getContentPane().add(statusBar, BorderLayout.SOUTH);
 		statusBar.add(statusBarMenu);
+		statusBar.setPreferredSize(new Dimension(900, 25));
 	}
-	
+
+	private void createToolsBar(JFrame frame) {
+		JMenuBar toolsBar = new JMenuBar();
+		frame.setJMenuBar(toolsBar);
+		toolsBar.setPreferredSize(new Dimension(900, 75));
+		this.createToolsBarItems(toolsBar);
+	}
+
+	private void createToolsBarItems(JMenuBar toolsBar) {
+		JMenuItem newFile = new JMenuItem("Abrir [ctrl + n]");
+		newFile.setHorizontalAlignment(SwingConstants.LEFT);
+		Image newFileIcon = new ImageIcon(this.getClass().getResource("/new_file_icon.png")).getImage();
+		toolsBar.add(newFile);
+		newFile.setIcon(new ImageIcon(newFileIcon));
+		newFile.setPreferredSize(new Dimension(10, 10));
+	}
+
 	private String getCurrentDirectory() {
 		return this.getClass().getClassLoader().getResource("").getPath();
 	}
